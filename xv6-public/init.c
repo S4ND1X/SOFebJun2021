3b30a5d4d9ed31ed1a8d1e99f18d191b54962b54
Jorge Sanchez <sandix390@gmail.com>
2021-03-20 03:54:50 +0000

    Practica 2

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,14 +20,16 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "Practica 2\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
       exit();
     }
     if(pid == 0){
-      exec("sh", argv);
+	  //1) Cuando paso a ls se queda en un for infinito pasando por las funciones
+      exec("programax", argv);
+	  //2) Cuando paso un programa inexistente EL EXEC falla por no encontrar el programa 
       printf(1, "init: exec sh failed\n");
       exit();
     }
